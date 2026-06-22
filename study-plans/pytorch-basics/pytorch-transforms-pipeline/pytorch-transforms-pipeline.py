45a8501896b1a38b01ca4cc1ad3cbd643712a945
Julia <jq2347@columbia.edu>
2026-06-22 11:32:44 -0400

    Solve pytorch-transforms-pipeline

---
+++ study-plans/pytorch-basics/pytorch-transforms-pipeline/pytorch-transforms-pipeline.py
@@ -0,0 +1,18 @@
+import torch
+
+class TransformPipeline:
+    """
+    Returns: float32 tensor of shape (C, H, W) from __call__
+    """
+
+    def __init__(self, mean, std):
+        self.mean = torch.tensor(mean)
+        self.std = torch.tensor(std)
+        
+
+    def __call__(self, image):
+        image = torch.tensor(image, dtype=torch.float32)
+        image = image/255
+        image = (image - self.mean)/self.std
+        image = image.permute(2,0,1)
+        return image
