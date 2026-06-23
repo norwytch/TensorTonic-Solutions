2b444a76313ed4683ead2102f80c4b0487508c52
Julia <jq2347@columbia.edu>
2026-06-23 14:05:59 -0400

    Solve pytorch-custom-linear-layer

--- study-plans/pytorch-basics/pytorch-custom-linear-layer/pytorch-custom-linear-layer.py
+++ study-plans/pytorch-basics/pytorch-custom-linear-layer/pytorch-custom-linear-layer.py
@@ -8,11 +8,9 @@ class CustomLinear(nn.Module):
 
     def __init__(self, in_features, out_features):
         super().__init__()
-        self.in_features = in_features
-        self.out_features = out_features
         self.weight = nn.Parameter(torch.randn(out_features, in_features))
-        self.bias = nn.Parameter(torch.randn(out_features))
-    
+        self.bias = nn.Parameter(torch.randn(out_features,))
+        
 
     def forward(self, x):
         y = x@self.weight.T + self.bias
