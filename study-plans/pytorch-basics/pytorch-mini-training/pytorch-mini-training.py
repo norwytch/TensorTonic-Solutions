b61c720edaa0411add9698f3986a8b8bb289eb51
Julia <jq2347@columbia.edu>
2026-06-23 18:08:23 -0400

    Solve pytorch-mini-training

--- study-plans/pytorch-basics/pytorch-mini-training/pytorch-mini-training.py
+++ study-plans/pytorch-basics/pytorch-mini-training/pytorch-mini-training.py
@@ -5,18 +5,18 @@ def train_epoch(model, dataloader, criterion, optimizer):
     """
     Returns: average loss over all batches (float)
     """
-    #forward, loss, backwards, step
     num_batches = len(dataloader)
-    acc_loss = 0.0
-    
+    accum_loss = 0.0
+
     for batch in dataloader:
         optimizer.zero_grad()
         inputs, targets = batch
         output = model(inputs)
         loss = criterion(output, targets)
-        acc_loss += loss.item()
+        accum_loss += loss
         loss.backward()
         optimizer.step()
 
-    avg_loss = acc_loss/num_batches
+    avg_loss = accum_loss/num_batches
     return avg_loss
+        
