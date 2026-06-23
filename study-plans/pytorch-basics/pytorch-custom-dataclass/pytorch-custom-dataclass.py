c5bf3dcecc745485f3c9a2383abb29cc07aff81c
Julia <jq2347@columbia.edu>
2026-06-23 16:53:46 -0400

    Solve pytorch-custom-dataclass

--- study-plans/pytorch-basics/pytorch-custom-dataclass/pytorch-custom-dataclass.py
+++ study-plans/pytorch-basics/pytorch-custom-dataclass/pytorch-custom-dataclass.py
@@ -8,7 +8,7 @@ class CSVDataset(Dataset):
 
     def __init__(self, data, label_col):
         self.data = data
-        self.label_col = label_col        
+        self.label_col = label_col
 
     def __len__(self):
         return len(self.data)
@@ -17,7 +17,8 @@ class CSVDataset(Dataset):
         row = self.data[idx]
         label = [row[self.label_col]]
         features = row[0:self.label_col] + row[self.label_col+1:]
-        features = torch.tensor(features, dtype=torch.float32)
-        label = torch.tensor(label, dtype=torch.float32)
 
+        features = torch.tensor(features, dtype=torch.float)
+        label = torch.tensor(label, dtype=torch.float)
+        
         return features,label
