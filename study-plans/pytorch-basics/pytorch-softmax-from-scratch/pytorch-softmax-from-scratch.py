1955d50885e2971cf281552af6b4832e45b21c4a
Julia <jq2347@columbia.edu>
2026-06-23 16:36:09 -0400

    Solve pytorch-softmax-from-scratch

--- study-plans/pytorch-basics/pytorch-softmax-from-scratch/pytorch-softmax-from-scratch.py
+++ study-plans/pytorch-basics/pytorch-softmax-from-scratch/pytorch-softmax-from-scratch.py
@@ -4,7 +4,8 @@ def softmax(logits):
     """
     Returns: tensor of same shape with softmax probabilities (each row sums to 1)
     """
-    m=logits.max(dim=1, keepdim=True).values
-    e = (logits - m).exp()
-    denominator = e.sum(dim=1, keepdim=True)
-    return e/denominator
+    m = logits.max(dim = 1, keepdim=True).values
+    numer = (logits - m).exp()
+    denom = numer.sum(dim = 1, keepdim=True)
+
+    return numer/denom
