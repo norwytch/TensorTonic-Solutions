b1b0c764b86779cf66c87d7a08b76b87ae175037
Julia <jq2347@columbia.edu>
2026-06-23 16:25:07 -0400

    Solve pytorch-loss-functions

--- study-plans/pytorch-basics/pytorch-loss-functions/pytorch-loss-functions.py
+++ study-plans/pytorch-basics/pytorch-loss-functions/pytorch-loss-functions.py
@@ -4,25 +4,27 @@ def compute_loss(pred, target, method, delta=1.0):
     """
     Returns: float, the mean loss value
     """
-
-    pred = torch.as_tensor(pred)
-    target = torch.as_tensor(target)
-
+    pred = torch.tensor(pred)
+    target = torch.tensor(target)
+    loss = None
+    N = pred.shape[0]
+    
     if method == 'mse':
         pred = pred.float()
         target = target.float()
-        loss = ((pred - target)**2).mean()
-        return loss.item()
-    
-    if method == 'cross_entropy':
-        N = pred.shape[0]
+        sqdiff = (target-pred)**2
+        loss = sqdiff.mean()
+
+    elif method == 'cross_entropy':
         lse = torch.logsumexp(pred, dim=1)
         correct = pred[torch.arange(N), target]
         loss = (lse - correct).mean()
-        return loss.item()
 
-    if method == 'huber':
+    elif method == 'huber':
         if delta is not None:
-            a = pred-target
-            loss = torch.where(torch.abs(a) <= delta, 0.5*(a**2), delta*(torch.abs(a) - .5*delta))
+            a = pred - target
+            loss = torch.where(torch.abs(a) <= delta, .5*(a**2), delta*(torch.abs(a) - .5*delta))
         return loss.mean().item()
+            
+    return loss.item()
+    
